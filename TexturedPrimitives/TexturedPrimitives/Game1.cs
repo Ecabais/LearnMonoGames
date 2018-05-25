68c80d1902b4f2c0177966969956ed8cb316b3df
unknown <elyvic.cabais01@utrgv.edu>
2018-05-24 23:03:14 -0500

    finished TexturedPrimitives Project

--- TexturedPrimitives/TexturedPrimitives/Game1.cs
+++ TexturedPrimitives/TexturedPrimitives/Game1.cs
@@ -1,81 +1,96 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using TexturedPrimitives.GraphicsSupport;
 
 namespace TexturedPrimitives
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        static public GraphicsDeviceManager graphics;
+        static public SpriteBatch spriteBatch;
+        static public ContentManager sContent;
+
+
+        //will be used to set window size
+        const int windowWidth = 1000;
+        const int windowHeight = 700;
+
+        const int NumObjects = 4;
+
+        TexturedPrimitive[] GraphicsObjects;
+
+        int CurrentIndex = 0;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            Game1.sContent = Content; //add this when having trouble with loading textures in texturedPrimitive class
+
+            graphics.PreferredBackBufferWidth = windowWidth;
+            graphics.PreferredBackBufferHeight = windowHeight;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            GraphicsObjects = new TexturedPrimitive[NumObjects];
+
+            GraphicsObjects[0] = new TexturedPrimitive("UWB-JPG", new Vector2(10f, 10f), new Vector2(30f, 30f));
+            GraphicsObjects[1] = new TexturedPrimitive("UWB-JPG", new Vector2(200, 200), new Vector2(100, 100));
+            GraphicsObjects[2] = new TexturedPrimitive("UWB-PNG", new Vector2(50, 10), new Vector2(30, 30));
+            GraphicsObjects[3] = new TexturedPrimitive("UWB-PNG", new Vector2(50, 200), new Vector2(100, 100));
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
 
-            // TODO: Add your update logic here
+            if (InputWrapper.Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (InputWrapper.Buttons.A == ButtonState.Pressed)
+                graphics.ToggleFullScreen();
+
+            if (InputWrapper.Buttons.X == ButtonState.Pressed)
+                CurrentIndex = (CurrentIndex + 1) % NumObjects;
+
+            GraphicsObjects[CurrentIndex].Update(InputWrapper.Thumbstick.Left, InputWrapper.Thumbstick.Right);
+
+            
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            Game1.spriteBatch.Begin();
+
+            foreach (TexturedPrimitive p in GraphicsObjects)
+            {
+                p.Draw();
+            }
+
+            Game1.spriteBatch.End();
 
             base.Draw(gameTime);
         }
